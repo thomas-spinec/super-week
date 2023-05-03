c8b91cc4d27bf2f18108afdcdb0ec8d7b298bd2c
thomas-spinec <thomas.spinec@laplateforme.io>
2023-05-03 15:23:10 +0200

    logout ok

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -131,4 +131,12 @@ class AuthController
             require 'src/View/login.php';
         }
     }
+
+    public function logout()
+    {
+        // suppression de la session
+        session_destroy();
+        // redirection vers la page d'accueil
+        header('Location: /super-week/');
+    }
 }
