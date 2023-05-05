001531e2dd41ccecdf51473984d8632e1f5fc919
thomas-spinec <thomas.spinec@laplateforme.io>
2023-05-05 16:10:03 +0200

    ajout des liens (git et portfolio)

--- src/View/book_form.php
+++ src/View/book_form.php
@@ -6,12 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style.css">
+    <!-- fav icon -->
+    <link rel="icon" type="image/png" href="/super-week/Logo_onglet.png" />
     <title>Ajout de livre</title>
 </head>
 
 <body>
     <header>
-        <h2>Projet Super-week</h2>
+        <h1><a href="/super-week">Projet Super-week</a></h1>
+        <div id="perso">
+            <a class="link" href="https://thomas-spinec.students-laplateforme.io/"><img src="/super-week/logo-thomas.png" alt="lien vers mon portfolio"></a>
+        </div>
+        <div class="logo">
+            <a class="link" href="https://github.com/thomas-spinec/super-week"><img src="/super-week/github.png" alt="lien vers le repo"></a>
+        </div>
     </header>
 
     <main>
